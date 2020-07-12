c4f08ecf1836c8979d3502c1dc51390fc8e937ad
Manasa Somisetty <manasa.somisetty06@gmail.com>
2020-07-12 19:07:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function src() {
+    if (document.getElementById('name').value != null && document.getElementById('add1').value != null && document.getElementById('add2').value != null && document.getElementById('postal').value != null &&
+        document.getElementById('city').value != null && document.getElementById('email').value != null && document.getElementById('select').value != '-1' && document.getElementById('phone').value != null) {
+        alert("Form Submitted With Values");
+    }
+}
